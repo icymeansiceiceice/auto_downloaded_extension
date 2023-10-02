62c590b54b81d3ddb5994d4057459e092d78727b
nOttOD4y <kaungmyat97k@gmail.com>
2023-10-02 21:22:19 +0630

    add login step

---
+++ content.js
@@ -0,0 +1,10 @@
+ let username = document.getElementById('inputEmail');
+ username.value='Aye Chan Htun Naing';
+ let password = document.getElementById('inputPassword');
+ password.value = 'l5ygn';
+
+let submit = document.querySelector('.btn-signin');
+
+setTimeout(()=>{
+    submit.click() ,5000
+});
